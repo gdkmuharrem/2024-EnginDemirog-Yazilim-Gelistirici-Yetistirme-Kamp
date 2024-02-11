fefe55faebdfdce185874ff1751e784c7153124c
ENIAC <ENIAC@DESKTOP-M9MCCUM>
2024-02-11 21:57:52 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,97 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+//variables -->camelCase
+bool isAuthenticated = true;
+Console.WriteLine(message1);
+
+//condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Buton-->Hoşgeldin Muharrem");
+}
+else
+{
+    Console.WriteLine("Buton-->Sisteme giriş yap");
+}
+
+string[] loans = {"Kredi 1","Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6" };//dbden gelecek
+//string[] loans2 = new string[6];
+//loans2[0] = "Kredi 1";
+
+      //start    condition        //increment
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+
+Console.WriteLine("kod bitti");
+
+IndividualCustomer customer1 = new IndividualCustomer();
+customer1.Id = 1;
+customer1.NationalIdentity = "12345678922";
+customer1.FirstName = "Muharrem";
+customer1.LastName = "Gedik";
+customer1.CustomerNumber = "123456";
+
+IndividualCustomer customer2 = new IndividualCustomer();
+customer2.Id = 2;
+customer2.NationalIdentity = "12512612512";
+customer2.FirstName = "Ahmet";
+customer2.LastName = "Yıldız";
+customer2.CustomerNumber = "123457";
+
+CoorporateCustomer customer3 = new CoorporateCustomer();
+customer3.Id = 3;
+customer3.Name = "Kodlama.io";
+customer3.TaxNumber = "12345678987";
+customer3.CustomerNumber = "123457";
+
+CoorporateCustomer customer4 = new CoorporateCustomer();
+customer4.Id = 4;
+customer4.Name = "Abc";
+customer4.TaxNumber = "12341248987";
+customer4.CustomerNumber = "125357";
+
+
+// values types => int,bool,double ... sayısal değerlerde değer tipi değişkenler denir.
+// referebce types => array,class,interface ...
+int number1 = 10;
+int number2 = 20;
+number1 = number2;
+number2 = 50;
+Console.WriteLine(number1);
+
+string[] cities1 = { "Ankara", "İstanbul", "İzmir" };
+string[] cities2 = { "Bursa", "Bolu", "Diyarbakır" };
+
+cities2 = cities1;
+cities1[0] = "Adana";
+
+Console.WriteLine(cities2[0]);
+
+//Polymorphism
+BaseCustomer[] customers = { customer1, customer2, customer3, customer4 };
+foreach (BaseCustomer customer in customers)
+{
+    Console.WriteLine(customer.CustomerNumber);
+}
+
+
